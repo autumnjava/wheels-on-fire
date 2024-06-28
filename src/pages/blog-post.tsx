b78fb3abba7cf4fa31dfbf73729f8cff40fb01dd
Alexander S <70532114+autumnjava@users.noreply.github.com>
2024-06-28 13:13:21 +0200

    sanity changes

--- src/pages/blog-post.tsx
+++ src/pages/blog-post.tsx
@@ -34,6 +34,12 @@ export const BlogPost = () => {
 
         return `<img src="${imageUrl}" alt="${altText}" class="block mx-auto" />`;
       },
+
+      youtube: ({ value }: any) => {
+        return `<iframe width="560" height="315" src=${value.url} title="YouTube video player"
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+      },
     },
     // marks: {
     //   link: ({ children, value }: any) => {
