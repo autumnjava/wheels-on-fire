81a836edf62848ab63f003c2fe20fde80331b033
Alexander S <70532114+autumnjava@users.noreply.github.com>
2024-06-28 13:16:26 +0200

    youtube sanity small change

--- src/pages/blog-post.tsx
+++ src/pages/blog-post.tsx
@@ -36,7 +36,7 @@ export const BlogPost = () => {
       },
 
       youtube: ({ value }: any) => {
-        return `<iframe width="560" height="315" src=${value.url} title="YouTube video player"
+        return `<iframe width="560" height="315" class="mx-auto" src=${value.url} title="YouTube video player"
         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
       },
