bca7bee5eda800a1f6ee375a4c8293e6216388ab
Alexander S <70532114+autumnjava@users.noreply.github.com>
2024-07-23 16:36:26 +0200

    outline thicker

--- src/components/slides/hero/hero-slide-2.tsx
+++ src/components/slides/hero/hero-slide-2.tsx
@@ -9,7 +9,7 @@ export const HeroSlide2 = (props: HeroProps) => {
       class="mb-8 flex h-[500px] w-full flex-col bg-black bg-cover bg-center px-12 pt-[8%] xl:pt-[5%] 2xl:pt-[2%] brightness-75 md:h-[600px] lg:h-[768px] md:px-16"
       style={{ 'background-image': `url(${bgImage})` }}
     >
-        <q class="text-outline block font-futura text-heroHeadingXL font-bold uppercase tracking-widest whitespace-nowrap">
+        <q class="text-outline-thicker block font-futura text-heroHeadingXL font-bold uppercase tracking-widest whitespace-nowrap">
           Forget the
           <br /> comfort, let's
           <br /> get dirty!
