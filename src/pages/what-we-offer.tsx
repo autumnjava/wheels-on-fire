531c32fb13642487092d9b56c44a46f77c6af3b0
Alexander S <70532114+autumnjava@users.noreply.github.com>
2024-08-21 10:39:07 +0200

    changes what we offer

--- src/pages/what-we-offer.tsx
+++ src/pages/what-we-offer.tsx
@@ -15,22 +15,47 @@ export const WhatWeOffer = () => {
   const halfDay = () => (
     <>
       <div class="what-we-offer my-4">
-        <p class="hidden sm:block">- Guided tour</p>
-        <p class="block sm:hidden">3-4 hrs - Guided tour</p>
+        <p class="">- Guided tour</p>
+        <p class="">- 3-4 hrs ride</p>
         <p>- Shuttle Service</p>
         <p>- Pick up - Drop off</p>
-        <br class="hidden sm:block" />
-        <p class="hidden sm:block">3-4 hrs</p>
       </div>
 
-      {/* <div class="what-we-offer-red my-4 flex justify-center whitespace-nowrap text-center text-red">
-        <p>* Min. 2 ppl</p>
-        <p class="ml-2">|</p>
-        <p class="ml-2">* Max. 3 ppl</p>
-      </div> */}
-      <div class="what-we-offer-red">
-        {/* <p class="text-red">* P/rider - 65€</p> */}
-        <p class="text-red">* Bike Rental - 55€</p>
+      <div class="what-we-offer-red my-4 justify-center text-center text-red">
+        <p>
+          Price p/person (with bike + protection gear -<br /> helmet, knee pads
+          and gloves) - 120€
+        </p>
+      </div>
+      <div class="mb-2 mt-4">
+        <p class="font-futuraMedium text-[19px] uppercase leading-[14px] sm:text-headingL sm:leading-[45px]">
+          Got your own bike?
+        </p>
+        <p class="my-2 font-futuraMedium text-[13px] leading-[110%] text-red sm:text-[20px] sm:leading-[25px]">
+          Price p/ person - 65€
+        </p>
+      </div>
+      <div class="what-we-offer-red my-2 justify-center text-center text-red">
+        <p>-</p>
+      </div>
+      <div class="my-2 flex justify-center whitespace-nowrap text-center ">
+        <p class="font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          * Min. 2 ppl
+        </p>
+        <p class="ml-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          |
+        </p>
+        <p class="ml-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          * Max. 3 ppl
+        </p>
+      </div>
+      <div class="my-4">
+        <Button
+          url="/get-in-touch"
+          customClass="block mx-auto !my-4 what-we-offer-get-in-touch uppercase"
+        >
+          Book now
+        </Button>
       </div>
     </>
   );
@@ -38,23 +63,47 @@ export const WhatWeOffer = () => {
   const fullDay = () => (
     <>
       <div class="what-we-offer my-4">
-        <p class="hidden sm:block">- Guided tour</p>
-        <p class="block sm:hidden">5-6 hrs - Guided tour</p>
+        <p class="">- Guided tour</p>
+        <p class="">- 5-6 hrs ride</p>
         <p>- Shuttle Service</p>
         <p>- Pick up - Drop off</p>
-        <br class="hidden sm:block" />
-        <p class="hidden sm:block">5-6 hrs</p>
       </div>
 
-      {/* <div class="what-we-offer-red my-4 flex justify-center whitespace-nowrap text-center text-red">
-        <p>* Min. 2 ppl</p>
-        <p class="ml-2">|</p>
-        <p class="ml-2">* Max. 3 ppl</p>
-      </div> */}
-
-      <div class="what-we-offer-red">
-        {/* <p class="text-red">* P/rider - 85€</p> */}
-        <p class="text-red">* Bike Rental - 75€</p>
+      <div class="what-we-offer-red my-4 justify-center text-center text-red">
+        <p>
+          Price p/person (with bike + protection gear -<br /> helmet, knee pads
+          and gloves) - 160€
+        </p>
+      </div>
+      <div class="mb-2 mt-4">
+        <p class="font-futuraMedium text-[19px] uppercase leading-[14px] sm:text-headingL sm:leading-[45px]">
+          Got your own bike?
+        </p>
+        <p class="my-2 font-futuraMedium text-[13px] leading-[110%] text-red sm:text-[20px] sm:leading-[25px]">
+          Price p/ person - 85€
+        </p>
+      </div>
+      <div class="what-we-offer-red my-2 justify-center text-center text-red">
+        <p>-</p>
+      </div>
+      <div class="my-2 flex justify-center whitespace-nowrap text-center ">
+        <p class="font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          * Min. 2 ppl
+        </p>
+        <p class="ml-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          |
+        </p>
+        <p class="ml-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          * Max. 3 ppl
+        </p>
+      </div>
+      <div class="my-4">
+        <Button
+          url="/get-in-touch"
+          customClass="block mx-auto !my-4 what-we-offer-get-in-touch uppercase"
+        >
+          Book now
+        </Button>
       </div>
     </>
   );
@@ -72,14 +121,32 @@ export const WhatWeOffer = () => {
 
       <div class="what-we-offer my-4">
         <p>- Guided tour</p>
+        <p class="">- 5-6 hrs ride</p>
         <p>- Shuttle Service</p>
         <p>- Pick up - Drop off</p>
       </div>
 
-      <div class="what-we-offer-red my-4 flex justify-center whitespace-nowrap text-center text-red">
-        <p>* Min. 2 ppl</p>
-        <p class="ml-2">|</p>
-        <p class="ml-2">* Max. 3 ppl</p>
+      <div class="what-we-offer-red my-2 justify-center text-center text-red">
+        <p>-</p>
+      </div>
+      <div class="my-2 flex justify-center whitespace-nowrap text-center ">
+        <p class="font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          * Min. 2 ppl
+        </p>
+        <p class="ml-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          |
+        </p>
+        <p class="ml-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+          * Max. 3 ppl
+        </p>
+      </div>
+      <div class="my-4">
+        <Button
+          url="/get-in-touch"
+          customClass="block mx-auto !my-4 what-we-offer-get-in-touch uppercase"
+        >
+          Book now
+        </Button>
       </div>
     </>
   );
@@ -119,6 +186,114 @@ export const WhatWeOffer = () => {
           first time out on a bike trail or if you are a full on dirt shredder,
           either way, youʼre in for a fun ride !
         </h2>
+      </div>
+
+      <div class="my-16 w-full border-t border-red text-center md:my-16">
+        <div class="w-[fit-content] bg-red px-8 py-1 font-futuraMedium text-[16px] uppercase leading-[100%] text-white sm:px-4 sm:text-headingM sm:leading-[27px]">
+          What we offer
+        </div>
+        <div class="mx-auto my-8 px-4">
+          <h3 class="font-futuraExtraBold text-headingXL uppercase leading-[100%]">
+            Either in for a half, full, or a couple of <br /> days you'll sure
+            feel your wheels on fire!
+          </h3>
+          <p class="my-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
+            * Ride will be adjusted to your needs & skills*
+          </p>
+
+          <div class="mx-auto mt-8 hidden max-w-[1070px] justify-between gap-4 text-center lg:flex">
+            <div>
+              <div class="mx-auto w-[fit-content] bg-red px-4 py-2 uppercase text-white">
+                <h4 class="font-futuraMedium text-headingS uppercase">
+                  Half day
+                </h4>
+              </div>
+
+              {halfDay()}
+            </div>
+
+            <div>
+              <div class="mx-auto w-[fit-content] bg-red px-4 py-2 uppercase text-white">
+                <h4 class="font-futuraMedium text-headingS uppercase">
+                  Full day
+                </h4>
+              </div>
+              {fullDay()}
+            </div>
+
+            <div>
+              <div class="mx-auto w-[fit-content] bg-red px-4 py-2 uppercase text-white">
+                <h4 class="font-futuraMedium text-headingS uppercase">
+                  1 day +
+                </h4>
+              </div>
+              {oneDayPlus()}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <ExpandableContainer heading="half day" customCss="block lg:hidden">
+        {halfDay()}
+      </ExpandableContainer>
+      <ExpandableContainer heading="full day" customCss="block lg:hidden">
+        {fullDay()}
+      </ExpandableContainer>
+      <ExpandableContainer heading="1 day +" customCss="block lg:hidden">
+        {oneDayPlus()}
+      </ExpandableContainer>
+
+      <div class="my-8 mt-16 flex w-full flex-col border-t border-red md:my-16">
+        <div class="w-[fit-content] bg-red px-8 py-1 font-futuraMedium text-[16px] uppercase leading-[100%] text-white sm:px-4 sm:text-headingM sm:leading-[27px]">
+          Customize your trip
+        </div>
+        <div class="mx-auto max-w-[650px] p-4 pt-12 text-center md:pt-8 [&>*:not(:first-child)]:my-2">
+          <h3 class="font-futuraExtraBold text-headingXL uppercase">
+            Do you want us to customize your trip?
+          </h3>
+          <h4 class="font-futuraMedium text-headingL uppercase">
+            Are you looking for a customized adventure where
+            <br class="hidden sm:block" /> youʼll get to see all the different
+            parts of the island?
+          </h4>
+          <p class="font-futuraMedium text-headingL">
+            * Ride will be adjusted to your needs & skills*
+          </p>
+          <p class="font-futuraMedium text-headingS text-red">* Min 1 day</p>
+
+          <Button url="/get-in-touch" customClass="block !mt-4 mx-auto">
+            Get in touch
+          </Button>
+        </div>
+      </div>
+
+      <div class="my-8 w-full border-t border-red text-center md:my-16">
+        <div class="w-[fit-content] bg-red px-8 py-1 font-futuraMedium text-[16px] uppercase leading-[100%] text-white sm:px-4 sm:text-headingM sm:leading-[27px]">
+          Private tour
+        </div>
+        <div class="mt-8 px-4 md:m-4 md:mt-4">
+          <h3 class="font-futuraExtraBold text-headingXL uppercase">
+            Are you the one <br /> and only rider?
+          </h3>
+          <Button url="/get-in-touch" customClass="block mx-auto !my-4">
+            Get in touch
+          </Button>
+        </div>
+      </div>
+
+      <div class="my-8 w-full border-t border-red text-center md:my-16">
+        <div class="w-[fit-content] bg-red px-8 py-1 font-futuraMedium text-[16px] uppercase leading-[100%] text-white sm:px-4 sm:text-headingM sm:leading-[27px]">
+          Bike rental | shuttle
+        </div>
+        <div class="mt-8 px-4 md:m-4 md:mt-4">
+          <h3 class="font-futuraExtraBold text-headingXL uppercase">
+            Do you just want to rent bikes? <br class="hidden sm:block" /> Or
+            shuttle?
+          </h3>
+          <Button url="/get-in-touch" customClass="block mx-auto !my-4">
+            Get in touch
+          </Button>
+        </div>
       </div>
 
       <div class="my-8 w-full border-t border-red md:my-16">
@@ -164,170 +339,28 @@ export const WhatWeOffer = () => {
         </div>
       </div>
 
-      <hr />
-
-      <div class="my-8 flex justify-center text-center md:my-16">
-        <div class="mx-8">
-          <p class="font-futuraMedium text-[1.25rem] uppercase leading-[60px] text-red sm:text-[30px]">
-            M
-          </p>
-          <p class="whitespace-nowrap font-andaleMonoBold text-[16px] leading-[27px]">
-            168 - 180 CM
-          </p>
-        </div>
-
-        <div class="mx-8">
-          <p class="font-futuraMedium text-[1.25rem] uppercase leading-[60px] text-red sm:text-[30px]">
-            L
-          </p>
-          <p class="whitespace-nowrap font-andaleMonoBold text-[16px] leading-[27px]">
-            178 - 190 CM
-          </p>
-        </div>
-      </div>
-
-      <div class="my-16 w-full border-t border-red text-center md:my-16">
-        <div class="w-[fit-content] bg-red px-8 py-1 font-futuraMedium text-[16px] uppercase leading-[100%] text-white sm:px-4 sm:text-headingM sm:leading-[27px]">
-          What we offer
-        </div>
-        <div class="mx-auto my-8 px-4">
-          <h3 class="font-futuraExtraBold text-headingXL uppercase leading-[85px]">
-            What's included
-          </h3>
-          <h4 class="font-futuraMedium uppercase md:text-headingL md:leading-[45px]">
-            Either in for a half, full or a couple of days
-            <br class="block md:hidden" /> youʼll sure feel your
-            <span class="font-futuraExtraBold"> WHEELS ON FIRE!</span>
-          </h4>
-          <p class="my-2 font-futuraMedium text-[19px] leading-[14px] sm:text-headingL sm:leading-[45px]">
-            * Ride will be adjusted to your needs & skills*
-          </p>
-
-          <p class="font-futuraMedium text-[17px] leading-[14px] text-red sm:text-headingM">
-            * Includes a full suspension Enduro bike, helmet & knee pads, gloves
-          </p>
-
-          <div class="mx-auto mt-8 hidden max-w-[1070px] justify-between gap-4 text-center lg:flex">
-            <div>
-              <div class="mx-auto w-[fit-content] bg-red px-4 py-2 uppercase text-white">
-                <h4 class="font-futuraMedium text-headingS uppercase">
-                  Half day
-                </h4>
-              </div>
-
-              {halfDay()}
-            </div>
-
-            <div>
-              <div class="mx-auto w-[fit-content] bg-red px-4 py-2 uppercase text-white">
-                <h4 class="font-futuraMedium text-headingS uppercase">
-                  Full day
-                </h4>
-              </div>
-              {fullDay()}
-            </div>
-
-            <div>
-              <div class="mx-auto w-[fit-content] bg-red px-4 py-2 uppercase text-white">
-                <h4 class="font-futuraMedium text-headingS uppercase">
-                  1 day +
-                </h4>
-              </div>
-              {oneDayPlus()}
-              <div class="hidden md:block">
-                <Button
-                  url="/get-in-touch"
-                  customClass="block mx-auto text-[17px] w-full"
-                >
-                  Get in touch
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <ExpandableContainer heading="half day" customCss="block lg:hidden">
-        {halfDay()}
-
-        <Button
-          url="/get-in-touch"
-          customClass="block mx-auto !my-4 what-we-offer-get-in-touch !px-[10px]"
-        >
-          Get in touch
-        </Button>
-      </ExpandableContainer>
-      <ExpandableContainer heading="full day" customCss="block lg:hidden">
-        {fullDay()}
-
-        <Button
-          url="/get-in-touch"
-          customClass="block mx-auto !my-4 what-we-offer-get-in-touch !px-[10px]"
-        >
-          Get in touch
-        </Button>
-      </ExpandableContainer>
-      <ExpandableContainer heading="1 day +" customCss="block lg:hidden">
-        {oneDayPlus()}
-
-        <Button
-          url="/get-in-touch"
-          customClass="block mx-auto !my-4 what-we-offer-get-in-touch !px-[10px]"
-        >
-          Get in touch
-        </Button>
-      </ExpandableContainer>
-
-      <div class="my-8 mt-16 flex w-full flex-col border-t border-red md:my-16">
-        <div class="w-[fit-content] bg-red px-8 py-1 font-futuraMedium text-[16px] uppercase leading-[100%] text-white sm:px-4 sm:text-headingM sm:leading-[27px]">
-          Customize your trip
-        </div>
-        <div class="mx-auto max-w-[650px] p-4 pt-12 text-center md:pt-8 [&>*:not(:first-child)]:my-2">
-          <h3 class="font-futuraExtraBold text-headingXL uppercase">
-            Do you want us to customize your trip?
-          </h3>
-          <h4 class="font-futuraMedium text-headingL uppercase">
-            Are you looking for a customized adventure where
-            <br class="hidden sm:block" /> youʼll get to see all the different
-            parts of the island?
-          </h4>
-          <p class="font-futuraMedium text-headingL">
-            * Ride will be adjusted to your needs & skills*
-          </p>
-          <p class="font-futuraMedium text-headingS text-red">* Min 1 day</p>
-
-          <Button url="/get-in-touch" customClass="block !mt-4 mx-auto">
-            Get in touch
-          </Button>
-        </div>
-      </div>
-
       <div class="my-8 w-full border-t border-red text-center md:my-16">
         <div class="w-[fit-content] bg-red px-8 py-1 font-futuraMedium text-[16px] uppercase leading-[100%] text-white sm:px-4 sm:text-headingM sm:leading-[27px]">
-          Private tour
+          Sizes
         </div>
-        <div class="mt-8 px-4 md:m-4 md:mt-4">
-          <h3 class="font-futuraExtraBold text-headingXL uppercase">
-            Are you the one <br /> and only rider?
-          </h3>
-          <Button url="/get-in-touch" customClass="block mx-auto !my-4">
-            Get in touch
-          </Button>
-        </div>
-      </div>
+        <div class="flex justify-center">
+          <div class="mx-8">
+            <p class="font-futuraMedium text-[1.25rem] uppercase leading-[60px] text-red sm:text-[30px]">
+              M
+            </p>
+            <p class="whitespace-nowrap font-andaleMonoBold text-[16px] leading-[27px]">
+              168 - 180 CM
+            </p>
+          </div>
 
-      <hr />
-
-      <div class="mt-8 w-full text-center md:mt-16">
-        <div class="mt-12 px-4">
-          <h3 class="font-futuraExtraBold text-headingXL uppercase">
-            Do you just want to rent bikes? <br class="hidden sm:block" /> Or
-            shuttle?
-          </h3>
-
-          <Button url="/get-in-touch" customClass="block mx-auto !mt-4">
-            Get in touch
-          </Button>
+          <div class="mx-8">
+            <p class="font-futuraMedium text-[1.25rem] uppercase leading-[60px] text-red sm:text-[30px]">
+              L
+            </p>
+            <p class="whitespace-nowrap font-andaleMonoBold text-[16px] leading-[27px]">
+              178 - 190 CM
+            </p>
+          </div>
         </div>
       </div>
     </>
